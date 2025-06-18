56d2c5d048f43677eaf490e3e9518faa98697d5e
sxkiss <admin@sxkiss.com>
2025-06-18 15:23:55 +0800

    删除无关文件

--- src/main/java/com/messageforwarder/Application.java
+++
@@ -1,5 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        org.springframework.boot.SpringApplication.run(Application.class, args);
-    }
-}
--- src/main/java/com/messageforwarder/core/MessageHandler.java
+++
@@ -1,8 +0,0 @@
-package com.messageforwarder.core;
-
-public class MessageHandler {
-
-    public void handleIncomingMessage(String message) {
-        // 处理接收到的消息的逻辑
-    }
-}
